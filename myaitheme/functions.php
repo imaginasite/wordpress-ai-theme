b760c36a473648abfba366d273d548bf6b67fee0
Anis MK <kouddane@gmail.com>
2025-09-17 12:56:21 +0100

    Update functions.php
    Removed some modifications i made after theme generation to keep it minimal.

--- myaitheme/functions.php
+++ myaitheme/functions.php
@@ -10,17 +10,3 @@ function myaitheme_scripts() {
     wp_enqueue_script( 'myaitheme-script', get_template_directory_uri() . '/assets/script.js', array(), null, true );
 }
 add_action( 'wp_enqueue_scripts', 'myaitheme_scripts' );
-
-/*****/
-// add_action( 'wp_enqueue_scripts', 'remove_global_styles' );
-// function remove_global_styles(){
-//     wp_dequeue_style( 'global-styles' );
-// }
-
-function mon_theme_enqueue_google_fonts() {
-    // Préchargement pour améliorer les performances
-    echo '<link rel="preconnect" href="https://fonts.googleapis.com">' . "\n";
-    echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' . "\n";
-    echo '<link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200..800&display=swap" rel="stylesheet">' . "\n";
-}
-add_action( 'wp_head', 'mon_theme_enqueue_google_fonts', 1 );
