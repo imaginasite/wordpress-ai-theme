b7e74f387f037fea8b304e256ccbab8d3c33d8fa
anismk <kouddane@gmail.com>
2026-05-11 19:39:47 +0100

    remove unnecessary files

--- myaitheme/test_php.php
+++
@@ -1,2 +0,0 @@
-<?php
-// Just want to check something if needed
--- myaitheme/test_register.php
+++
@@ -1,4 +0,0 @@
-<?php
-require 'wp-load.php';
-register_block_style( 'core/button', array( 'name' => 'fill', 'label' => 'Fill' ) );
-echo "Success\n";
